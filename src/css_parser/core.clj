50ad2c9c84144fb18cf75c878a3fb90ddc2ea0cb
akthrms <akt.hrms@gmail.com>
2020-08-28 01:53:21 +0900

    update

--- src/css_parser/core.clj
+++ src/css_parser/core.clj
@@ -1,1 +1,20 @@
 (ns css-parser.core)
+
+(defn any [input]
+  (if-let [c (first input)]
+    (list [c (.substring input 1)])
+    '()))
+
+(defn failure [_] '())
+
+; (any "clojure-1.9")
+; => ([\c "lojure-1.9"])
+
+(defn parse [parser input]
+  (parser input))
+
+(defn parse-all [parser input]
+  (->> input
+       (parse parser)
+       (filter #(= "" (second %)))
+       (ffirst)))
